7c754950cdf151323afc5ac50c0210459e4b4d44
ramazan97 <52708750+ramazan97@users.noreply.github.com>
2024-02-04 21:50:17 +0300

    tailwindcss- react app in kurulumu

---
+++ adminpanel/src/App.js
@@ -0,0 +1,11 @@
+import "./App.css";
+
+function App() {
+  return (
+    <div className="bg-gray-900">
+      <h1 className="text-3xl font-bold text-w underline">Hello world!</h1>
+    </div>
+  );
+}
+
+export default App;
