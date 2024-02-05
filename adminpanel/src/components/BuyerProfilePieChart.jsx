5daced62c676c43fec1e4628fb101fcc748835c8
ramazan97 <52708750+ramazan97@users.noreply.github.com>
2024-02-05 22:47:42 +0300

    Proje dosyalarının, app-router olayının oluşturulması, package.json importlarının yapılması

---
+++ adminpanel/src/components/BuyerProfilePieChart.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const BuyerProfilePieChart = () => {
+  return (
+	<div>BuyerProfilePieChart</div>
+  )
+}
+
+export default BuyerProfilePieChart
---
+++ adminpanel/src/components/DashboardStatsGrid.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const DashboardStatsGrid = () => {
+  return (
+	<div>DashboardStatsGrid</div>
+  )
+}
+
+export default DashboardStatsGrid
