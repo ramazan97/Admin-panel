53ed7b60deebb7e2865e3834292141f9817b57cc
ramazan97 <52708750+ramazan97@users.noreply.github.com>
2024-02-07 21:21:11 +0300

    Admin panel tamamlandı
    (https://www.youtube.com/watch?v=cFZ57R3H1GM&list=PLwHsNjmBZ-MJnVNyiaVYHW_dDA5n3mbVk&index=3&t=3s)

--- adminpanel/src/lib/constants/index.jsx
+++ adminpanel/src/lib/constants/index.jsx
@@ -1,9 +1,64 @@
-import React from 'react'
+import {
+	HiOutlineViewGrid,
+	HiOutlineCube,
+	HiOutlineShoppingCart,
+	HiOutlineUsers,
+	HiOutlineDocumentText,
+	HiOutlineAnnotation,
+	HiOutlineQuestionMarkCircle,
+	HiOutlineCog
+} from 'react-icons/hi'
 
-const index = () => {
-  return (
-	<div>index</div>
-  )
-}
+export const DASHBOARD_SIDEBAR_LINKS = [
+	{
+		key: 'dashboard',
+		label: 'Dashboard',
+		path: '/',
+		icon: <HiOutlineViewGrid />
+	},
+	{
+		key: 'products',
+		label: 'Products',
+		path: '/products',
+		icon: <HiOutlineCube />
+	},
+	{
+		key: 'orders',
+		label: 'Orders',
+		path: '/orders',
+		icon: <HiOutlineShoppingCart />
+	},
+	{
+		key: 'customers',
+		label: 'Customers',
+		path: '/customers',
+		icon: <HiOutlineUsers />
+	},
+	{
+		key: 'transactions',
+		label: 'Transactions',
+		path: '/transactions',
+		icon: <HiOutlineDocumentText />
+	},
+	{
+		key: 'messages',
+		label: 'Messages',
+		path: '/messages',
+		icon: <HiOutlineAnnotation />
+	}
+]
 
-export default index
+export const DASHBOARD_SIDEBAR_BOTTOM_LINKS = [
+	{
+		key: 'settings',
+		label: 'Settings',
+		path: '/settings',
+		icon: <HiOutlineCog />
+	},
+	{
+		key: 'support',
+		label: 'Help & Support',
+		path: '/support',
+		icon: <HiOutlineQuestionMarkCircle />
+	}
+]
